7e8de2770dba8a471f7c98b1c73ef1248d215500
Gerson Rodrigues <rodrigues87gerson@gmail.com>
2021-08-24 05:18:51 -0300

    atualizado

--- desafio03.py
+++ desafio03.py
@@ -1,4 +1,5 @@
-a = input ('Digite um número')
-b = input ('Agora outro')
+a = int(input ('Digite um número'))
+b = int(input ('Agora outro'))
 soma =  a+b
-print('A soma é',soma)
+#print('A soma entre ',a,'e',b,'é',soma)
+print('A soma entre {} e {} vale {}'.format(a,b,soma))
